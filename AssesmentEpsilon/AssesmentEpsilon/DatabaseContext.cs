fb9e6374dd427f43c0390cc444f3b41489d675b6
Petsinas Konstantinos <petkostas@hotmail.com>
2024-02-28 22:06:19 +0200

    EF install DBcontext

---
+++ AssesmentEpsilon/AssesmentEpsilon/DatabaseContext.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace AssesmentEpsilon
+{
+    public class DatabaseContext : DbContext
+    {
+        public DbSet<Customer> Customers { get; set; }
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseSqlServer("Server=localhost\\SQLEXPRESS;Database=master;Trusted_Connection=True;");
+        }
+    }
+}
