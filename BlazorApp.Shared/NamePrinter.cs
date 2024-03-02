3b35c2c6ed45e62ab6c986bdde129c8d65fc1761
Petsinas Konstantinos <petkostas@hotmail.com>
2024-03-02 14:26:53 +0200

    Name Printer

---
+++ BlazorApp.Shared/NamePrinter.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BlazorApp.Shared
+{
+    public class NamePrinter
+    {
+        public void PrintName(IPerson person)
+        {
+            Console.WriteLine(person.Name );
+        }
+    }
+}
